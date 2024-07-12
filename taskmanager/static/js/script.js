c173c91d435cb597f8d088debb06875f6fa1ac07
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-12 13:21:02 +0000

    ADD: Modal to confirm user is sure they want to delete a category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  //modal
+  let deleteModal = document.querySelectorAll('.modal');
+  instances = M.Modal.init(deleteModal);
+});
